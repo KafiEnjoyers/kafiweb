5e1534baf25298bbe8781b215109c2bf55ff172c
Yuhao-Sun <sunyh0917@outlook.com>
2025-04-08 00:56:20 +0800

    修复 GitHub Pages 部署配置

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,8 @@ const config = {
       pages: 'build',
       assets: 'build',
       fallback: 'index.html',
-      precompress: false
+      precompress: false,
+      strict: true
     }),
     paths: {
       base: dev ? '' : '/kafiweb'
