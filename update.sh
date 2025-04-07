e769b0220a2425b4aa795fd4c41248ba901d34bf
Yuhao-Sun <sunyh0917@outlook.com>
2025-04-08 01:09:32 +0800

    更新依赖版本，修复GitHub Pages部署配置

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 删除package-lock.json和node_modules
+rm -rf package-lock.json node_modules
+
+# 重新安装依赖
+npm install
+
+# 提交更改
+git add .
+git commit -m "更新依赖版本，修复GitHub Pages部署配置"
+
+# 推送到GitHub
+git push origin main
+
+echo "更新完成！"
